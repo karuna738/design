3ea798da35fe0846bd94a6d91a3e3a3b8d61e788
karuna738 <karankarna738@gmail.com>
2024-09-29 11:23:02 +0530

    'authGuard-implements'

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit{
   router = inject(Router);
+  constructor(private authService: AuthService) {}
+  username: string = '';
+  password: string = '';
   ngOnInit() {
   }
   onLogin() {
-    console.log('login');
-    this.router.navigateByUrl('dashboard');
+    if (this.authService.login(this.username, this.password)) {
+      // Redirect to protected route
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid username or password');
+    }
+  
   }
 
 }
