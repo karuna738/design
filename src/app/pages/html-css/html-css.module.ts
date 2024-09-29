a6c420bdc27098b2e1b6a15e6c9d8d99242e01ff
karuna738 <karankarna738@gmail.com>
2024-09-29 18:32:24 +0530

    'task-1

---
+++ src/app/pages/html-css/html-css.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HtmlCssRoutingModule } from './html-css-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HtmlCssRoutingModule
+  ]
+})
+export class HtmlCssModule { }
---
+++ src/app/pages/html-css/task-1/task-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-1.component.html',
+  styleUrl: './task-1.component.scss'
+})
+export class Task1Component {
+
+}
