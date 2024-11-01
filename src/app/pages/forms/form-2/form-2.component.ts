9d2137bc1990d14d47c7cf7926d3eb493af6709d
karuna738 <karankarna738@gmail.com>
2024-11-01 08:26:27 +0530

    'nov-one'

---
+++ src/app/pages/forms/form-2/form-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-2.component.html',
+  styleUrl: './form-2.component.scss'
+})
+export class Form2Component {
+
+}
