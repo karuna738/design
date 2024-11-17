0c0920b916bed7c238b8e128acb9982e7c7d5abb
karuna738 <karankarna738@gmail.com>
2024-11-17 20:19:22 +0530

    'formswork'

--- src/app/pages/forms/form-1/form-1.component.ts
+++ src/app/pages/forms/form-1/form-1.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-1.component.html',
-  styleUrl: './form-1.component.scss'
+  styleUrl: './form-1.component.scss',
 })
-export class Form1Component {
+export class Form1Component implements OnInit {
+  myform!: FormGroup;
+  public submited:boolean = false;
+  total: any = 0;
+  monthlyPayment: any = 0;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.myform = this.fb.group({
+      Amount: ['',Validators.required],
+      Term  : ['',Validators.required],
+      Rate  : ['',Validators.required],
+      Type : ['0']
+    });
+  }
+  calculatePayment(amount: number, interestRate: number, numberOfPayments: number): number {
+    const numerator = interestRate * amount * Math.pow(1 + interestRate, numberOfPayments);
+    const denominator = Math.pow(1 + interestRate, numberOfPayments) - 1;
+    return numerator / denominator;
+  }
+  onSubmit(){
+    this.submited = true;
+    if(this.myform.valid){
+      const amount = this.myform.get('Amount')?.value;
+      const term = this.myform.get('Term')?.value;
+      const rate = this.myform.get('Rate')?.value;
+      const type = this.myform.get('Type')?.value;
+      let intrate = parseFloat(rate) / 100;
+      let n = term * 12;
+      this.monthlyPayment = this.calculatePayment(amount, intrate, n).toFixed(2);
+      this.total =( this.monthlyPayment * n).toFixed(2);
+
+    }
+  }
+  reSetData(){
+    this.submited = false;
+    this.monthlyPayment =  0;
+    this.total = 0;
+    this.myform.reset();
+  }
 }
